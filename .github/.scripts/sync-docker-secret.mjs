9484f6abf027b51f9b0e42c78fb501e4024729d6
MuXiu1997 <muxiu1997@gmail.com>
2021-12-24 19:02:39 +0800

    style: 💄 prettier

--- .github/.scripts/sync-docker-secret.mjs
+++ .github/.scripts/sync-docker-secret.mjs
@@ -1,15 +1,21 @@
 #!/usr/bin/env zx
 
-const {sshHost, sshUsername} = argv
+const { sshHost, sshUsername } = argv
 
 const DOCKER_SECRET_PREFIX = 'DOCKER_SECRET_'
 
-const keys = Object.keys(process.env).filter(k => k.startsWith(DOCKER_SECRET_PREFIX))
+const keys = Object.keys(process.env).filter((k) =>
+  k.startsWith(DOCKER_SECRET_PREFIX)
+)
 
-const script = keys.map(k => {
-  const secretKey = k.slice(DOCKER_SECRET_PREFIX.length)
-  return `docker secret rm ${secretKey}
+const script = keys
+  .map((k) => {
+    const secretKey = k.slice(DOCKER_SECRET_PREFIX.length)
+    return `docker secret rm ${secretKey}
   echo '${process.env[k]}' | docker secret create ${secretKey} -`
-}).join('\n')
+  })
+  .join('\n')
 
-await nothrow($`ssh -o StrictHostKeyChecking=no ${sshUsername}@${sshHost} ${script}`)
+await nothrow(
+  $`ssh -o StrictHostKeyChecking=no ${sshUsername}@${sshHost} ${script}`
+)
