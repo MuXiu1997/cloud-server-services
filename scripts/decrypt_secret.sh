89e05d3ea3b0e0d9af27b42737aaf375d023a210
MuXiu1997 <muxiu1997@gmail.com>
2022-09-10 21:33:08 +0800

    Add secret.yml.age

---
+++ scripts/decrypt_secret.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+curl -fsSL http://kube.muxiu1997.com/secret.yml.age | age -d -i $HOME/.local/share/age_secret_key
---
+++ scripts/encrypt_secret.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+age -R $HOME/.local/share/age_public_key -a -o secret.yml.age -- secret.yml
