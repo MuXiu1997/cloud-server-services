6862b334dbe8c709e85a129bd8badf88c13f613a
MuXiu1997 <muxiu1997@gmail.com>
2023-01-26 17:15:51 +0800

    Update k3s server manifests
    add traefik-github-oauth-plugin

--- scripts/generate_server_manifests.sh
+++ scripts/generate_server_manifests.sh
@@ -6,5 +6,8 @@ curl -fsSL http://kube.muxiu1997.com/traefik-secret.yaml.age \
   | age -d \
   | sudo tee /var/lib/rancher/k3s/server/manifests/traefik-secret.yaml > /dev/null
 
+curl -fsSL http://kube.muxiu1997.com/traefik-github-oauth-server.yaml \
+  | sudo tee /var/lib/rancher/k3s/server/manifests/traefik-github-oauth-server.yaml > /dev/null
+
 curl -fsSL http://kube.muxiu1997.com/traefik-config.yaml \
   | sudo tee /var/lib/rancher/k3s/server/manifests/traefik-config.yaml > /dev/null
